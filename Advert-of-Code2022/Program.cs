4d61698e065e4c0224c1c36d7ddbd3aacbb9b658
Alvin strandberg <Str4ndb3rg@live.se>
2022-12-05 08:14:07 +0100

    AoC 01 done.

--- Advert-of-Code2022/Program.cs
+++ Advert-of-Code2022/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
-namespace Advert_of_Code2022
+namespace AoC_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Key variables.
+            string path = @"C:\Users\Alvin\Desktop\elfStats.txt";
+
+            StreamReader sr = new StreamReader(path);
+
+            List<int> elfsCalories = new List<int>();
+
+            int elfCalorie = 0;
+
+            for (int i = 0; i < 2500; i++)
+            {
+                string input = sr.ReadLine();
+
+                if (input != "")
+                {
+                    elfCalorie += Convert.ToInt32(input);
+                }
+                if (input == "")
+                {
+                    elfsCalories.Add(Convert.ToInt32(elfCalorie));
+                    elfCalorie = 0;
+                }
+            }
+            sr.Close();
+            Console.WriteLine(elfsCalories.Max());
+
+            int topCalories = 0;
+            int counter = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                counter++;
+                var index = elfsCalories.IndexOf(elfsCalories.Max());
+
+                topCalories += elfsCalories[index];
+                elfsCalories.RemoveAt(index);
+
+                Console.WriteLine(topCalories + " - Counter: " + counter);
+            }
+
+
+
         }
     }
 }
